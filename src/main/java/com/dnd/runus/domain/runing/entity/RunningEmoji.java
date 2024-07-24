e7912233714fe3b8daf7af4544378dca1420fb7d
hee9841 <lee98417716@gmail.com>
2024-07-24 23:24:35 +0900

    Feat: RunningEmoji 엔티티 추가

---
+++ src/main/java/com/dnd/runus/domain/runing/entity/RunningEmoji.java
@@ -0,0 +1,25 @@
+package com.dnd.runus.domain.runing.entity;
+
+import com.dnd.runus.domain.common.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class RunningEmoji extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imagePath;
+}
