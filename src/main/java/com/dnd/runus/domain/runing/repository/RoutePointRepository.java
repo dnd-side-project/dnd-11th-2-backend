9fbea81470e5f57363cc135bd202c6b2e2c89d8d
hee9841 <lee98417716@gmail.com>
2024-07-24 23:24:52 +0900

    Feat: RoutePoint 엔티티 추가

---
+++ src/main/java/com/dnd/runus/domain/runing/repository/RoutePointRepository.java
@@ -0,0 +1,6 @@
+package com.dnd.runus.domain.runing.repository;
+
+import com.dnd.runus.domain.runing.entity.RoutePoint;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoutePointRepository extends JpaRepository<RoutePoint, Long> {}
