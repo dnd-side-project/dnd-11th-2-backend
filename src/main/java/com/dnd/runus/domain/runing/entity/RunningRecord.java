24b66c972c0ac74d5988f7f61b7f2c02e99bc10d
sungHeeLee <70899677+hee9841@users.noreply.github.com>
2024-07-26 23:53:53 +0900

    Feat: PostGIS 의존성 추가, 러닝 엔티티 추가 (#22)
    * Chore: PostGIS 의존성 추가
    
    * Chore: PostGIS 관련 옵션 application.yml에 추가
    
    * Feat: RunningRecord 엔티티 추가
    
    * Feat: RunningEmoji 엔티티 추가
    
    * Feat: RoutePoint 엔티티 추가
    
    * Style: PostGIS 의존성 위치 수정
    
    * Feat: RunningRecord, RunningEmoji 테이블 리뷰 사항 반영

---
+++ src/main/java/com/dnd/runus/domain/runing/entity/RunningRecord.java
@@ -0,0 +1,87 @@
+package com.dnd.runus.domain.runing.entity;
+
+import com.dnd.runus.domain.common.BaseTimeEntity;
+import com.dnd.runus.domain.member.entity.Member;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.LineString;
+
+import java.time.Instant;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class RunningRecord extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
+    @NotNull
+    private Double distance;
+
+    @NotNull
+    private Double calorie;
+
+    @NotNull
+    private Double averagePace;
+
+    @NotNull
+    private Instant startAt;
+
+    @NotNull
+    private Instant endAt;
+
+    @Nullable
+    @Column(columnDefinition = "geometry(LineString, 4326)")
+    private LineString route;
+
+    @NotNull
+    private String location;
+
+    @NotNull
+    private Long emojiId;
+
+    @Column(length = 500)
+    private String goalDescription;
+
+    @Builder
+    private RunningRecord(
+            Member member,
+            Double distance,
+            Double calorie,
+            Double averagePace,
+            Instant startAt,
+            Instant endAt,
+            LineString route,
+            String location,
+            Long emojiId,
+            String goalDescription) {
+        this.member = member;
+        this.distance = distance;
+        this.calorie = calorie;
+        this.averagePace = averagePace;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.route = route;
+        this.location = location;
+        this.emojiId = emojiId;
+        this.goalDescription = goalDescription;
+    }
+}
