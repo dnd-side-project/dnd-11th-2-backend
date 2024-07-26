7e0cb3cc8367a55b62885fbfebdc7ec0c5855a0c
Jaewon Lee <jaewonlee.pro@gmail.com>
2024-07-26 09:35:55 +0900

    Feat: badge 엔티티 추가

---
+++ src/main/java/com/dnd/runus/domain/badge/entity/BadgeAchievement.java
@@ -0,0 +1,34 @@
+package com.dnd.runus.domain.badge.entity;
+
+import com.dnd.runus.domain.member.entity.Member;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class BadgeAchievement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = LAZY)
+    private Badge badge;
+
+    @NotNull
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
+    public static BadgeAchievement of(Badge badge, Member member) {
+        BadgeAchievement badgeAchievement = new BadgeAchievement();
+        badgeAchievement.badge = badge;
+        badgeAchievement.member = member;
+        return badgeAchievement;
+    }
+}
