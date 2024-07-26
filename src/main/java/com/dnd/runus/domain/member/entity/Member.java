1385e3b0a5ec298a36438a760f457434f564ddb6
Jaewon Lee <jaewonlee.pro@gmail.com>
2024-07-26 10:00:54 +0900

    Feat: level 엔티티 추가

--- src/main/java/com/dnd/runus/domain/member/entity/Member.java
+++ src/main/java/com/dnd/runus/domain/member/entity/Member.java
@@ -30,6 +30,9 @@ public class Member extends BaseTimeEntity {
 
     private Long badgeId;
 
+    @NotNull
+    private Integer exp;
+
     @Embedded
     @Accessors(fluent = true)
     private SocialProfile social;
