7b64b0c797d32bc0f47841cb81ca59cd6432ebd9
Jaewon Lee <jaewonlee.pro@gmail.com>
2024-08-08 23:49:01 +0900

    Style: PublicKeyProvider class의 Component 등록 삭제

--- src/main/java/com/dnd/runus/auth/oidc/provider/PublicKeyProvider.java
+++ src/main/java/com/dnd/runus/auth/oidc/provider/PublicKeyProvider.java
@@ -4,7 +4,6 @@ import com.dnd.runus.client.vo.OidcPublicKey;
 import com.dnd.runus.client.vo.OidcPublicKeyList;
 import com.dnd.runus.global.exception.BusinessException;
 import com.dnd.runus.global.exception.type.ErrorType;
-import org.springframework.stereotype.Component;
 
 import java.math.BigInteger;
 import java.security.KeyFactory;
@@ -15,21 +14,20 @@ import java.security.spec.RSAPublicKeySpec;
 import java.util.Base64;
 import java.util.Map;
 
-@Component
-public class PublicKeyProvider {
+public final class PublicKeyProvider {
 
-    public PublicKey generatePublicKey(final Map<String, String> tokenHeaders, final OidcPublicKeyList publicKeys) {
+    public static PublicKey generatePublicKey(Map<String, String> tokenHeaders, OidcPublicKeyList publicKeys) {
 
         OidcPublicKey publicKey = publicKeys.getMatchedKeyBy(tokenHeaders.get("kid"), tokenHeaders.get("alg"));
 
         return getPublicKey(publicKey);
     }
 
-    private PublicKey getPublicKey(final OidcPublicKey key) {
+    private static PublicKey getPublicKey(OidcPublicKey key) {
 
         try {
-            final byte[] nByte = Base64.getUrlDecoder().decode(key.n());
-            final byte[] eByte = Base64.getUrlDecoder().decode(key.e());
+            byte[] nByte = Base64.getUrlDecoder().decode(key.n());
+            byte[] eByte = Base64.getUrlDecoder().decode(key.e());
 
             BigInteger n = new BigInteger(1, nByte);
             BigInteger e = new BigInteger(1, eByte);
