c5bc69b4023c48865db419bf60d7ef91d63d6591
hee9841 <lee98417716@gmail.com>
2024-08-12 18:03:27 +0900

    Docs: 오늘의 챌린지 조회 api문서 추가

--- src/main/java/com/dnd/runus/presentation/v1/challenge/dto/response/ChallengesResponse.java
+++ src/main/java/com/dnd/runus/presentation/v1/challenge/dto/response/ChallengesResponse.java
@@ -2,14 +2,19 @@ package com.dnd.runus.presentation.v1.challenge.dto.response;
 
 
 import com.dnd.runus.domain.challenge.Challenge;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.jetbrains.annotations.NotNull;
 
 public record ChallengesResponse(
+    @Schema(example = "1")
     @NotNull
     Long challengeId,
+    @Schema(description = "챌린지 이름")
     @NotNull
     String name,
+    @Schema(description = "예상 소요 시간(단위 초), 페이스 관련 챌린지이면 0리턴")
     Integer expectedTime,
+    @Schema(description = "챌린지 이미지 URL")
     @NotNull
     String imageUrl
 )
