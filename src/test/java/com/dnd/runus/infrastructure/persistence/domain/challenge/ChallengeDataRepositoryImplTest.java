d0b4175b42cd54c4652118f070a1d63a5e8383c3
hee9841 <lee98417716@gmail.com>
2024-08-27 16:26:31 +0900

    Test: 챌린지 관련 테이블 변경으로 테스트 수정

--- src/test/java/com/dnd/runus/infrastructure/persistence/domain/challenge/ChallengeDataRepositoryImplTest.java
+++ src/test/java/com/dnd/runus/infrastructure/persistence/domain/challenge/ChallengeDataRepositoryImplTest.java
@@ -1,8 +1,9 @@
 package com.dnd.runus.infrastructure.persistence.domain.challenge;
 
-import com.dnd.runus.domain.challenge.Challenge;
+import com.dnd.runus.domain.challenge.ChallengeData;
 import com.dnd.runus.domain.challenge.ChallengeRepository;
 import com.dnd.runus.infrastructure.persistence.annotation.RepositoryTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RepositoryTest
-public class ChallengeRepositoryImplTest {
+public class ChallengeDataRepositoryImplTest {
 
     @Autowired
     private ChallengeRepository challengeRepository;
@@ -24,10 +25,10 @@ public class ChallengeRepositoryImplTest {
         boolean hasYesterdayRecord = true;
 
         // when
-        List<Challenge> challenges = challengeRepository.getChallenges(hasYesterdayRecord);
+        List<ChallengeData.Challenge> challenges = challengeRepository.findAllChallenges(hasYesterdayRecord);
 
         // then
-        assertTrue(challenges.stream().anyMatch(Challenge::isDefeatYesterdayChallenge));
+        assertTrue(challenges.stream().anyMatch(ChallengeData.Challenge::isDefeatYesterdayChallenge));
     }
 
     @DisplayName("어제 기록이 없는 경우 챌린지 리스트 조회 : 챌린지 타입 중 어제의 기록과 관련된 타입이 없어야 한다.")
@@ -37,15 +38,22 @@ public class ChallengeRepositoryImplTest {
         boolean hasYesterdayRecord = false;
 
         // when
-        List<Challenge> challenges = challengeRepository.getChallenges(hasYesterdayRecord);
+        List<ChallengeData.Challenge> challenges = challengeRepository.findAllChallenges(hasYesterdayRecord);
 
         // then
-        assertTrue(challenges.stream().noneMatch(Challenge::isDefeatYesterdayChallenge));
+        assertTrue(challenges.stream().noneMatch(ChallengeData.Challenge::isDefeatYesterdayChallenge));
     }
 
     @DisplayName("challengeId로 오늘의 챌린지 찾기")
     @Test
     void findChallengeById() {
-        assertTrue(challengeRepository.findById(1L).isPresent());
+
+        long challengeId = 1;
+
+        ChallengeData challengeDataWithConditions =
+                challengeRepository.findChallengeWithConditionsByChallengeId(challengeId);
+
+        Assertions.assertThat(challengeDataWithConditions.challengeConditions().size())
+                .isGreaterThan(0);
     }
 }
