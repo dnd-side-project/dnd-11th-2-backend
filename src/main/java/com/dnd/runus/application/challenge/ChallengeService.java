2ec00e23dec70fc969ba0d8b9841b969322b53e9
hee9841 <lee98417716@gmail.com>
2024-08-28 01:28:31 +0900

    Comment: 챌린지 성취 저장, 조회에 관한 코멘트(TODO로 표시)

--- src/main/java/com/dnd/runus/application/challenge/ChallengeService.java
+++ src/main/java/com/dnd/runus/application/challenge/ChallengeService.java
@@ -45,14 +45,16 @@ public class ChallengeService {
                 .toList();
     }
 
-    // 챌린지 성취 로직(러닝 서비스쪽으로 옮기면 될 것 같아요.
+    // TODO 러닝 저장 후, 사용자 별 챌린지 성취 기록 저장 시 아래 saveTest(), getChallengeAchievement() 를 참고 하세요.
+    //  saveTest(), getChallengeAchievement()의 주석을 참고해주세요.
     public void saveTest(RunningRecord runningRecord, Member member) {
-        // challenge 조건 까지 select
+        // 1.challenge의 조건 까지 select
         ChallengeWithCondition challenge =
                 challengeRepository.findChallengeWithConditionsByChallengeId(member.memberId());
 
+        // 2. 어제의 기록을 이기는 챌린지면
         if (challenge.challengeInfo().isDefeatYesterdayChallenge()) {
-            // 어제 기록 확인
+            // 3. 어제 기록 확인
             OffsetDateTime todayMidnight = LocalDate.now(SERVER_TIMEZONE_ID)
                     .atStartOfDay(SERVER_TIMEZONE_ID)
                     .toOffsetDateTime();
@@ -61,22 +63,33 @@ public class ChallengeService {
                     .findByMemberIdAndStartAtBetween(member.memberId(), yesterday, todayMidnight)
                     .get(0);
 
+            // 4. 어제 기록과 챌린지 목표값을 계산해서 해당 챌린지에 달성해야 하는 비교값(comparisonValue) 등록
             challenge
                     .challengeConditions()
                     .forEach(condition -> condition.registerComparisonValue(
                             condition.goalType().getActualValue(yesterdayRecord)));
         }
 
-        // 아래 리턴 값으로 챌린지 성취와와 퍼센테이지 저장
+        // 5. runningRecord와 ChallengeWithCondition으로 사용자의 챌린지 성취 기록 반환
         getChallengeAchievement(runningRecord, challenge);
+
+        // 6. 5에서 반환한 값으로 챌린지 성취 기록 저장
+        // ChallengeAchievementRepository의
+        // ChallengeAchievement save(ChallengeAchievement challengeAchievement); 함수로 저장
+
+        // 7. ChallengeAchievementRecord의 percentageValues가 null이 아닐 경우
+        // 7-1. 퍼센티이지 값 저장
+        //      ChallengeAchievementPercentageRepository의  PercentageValues save(..)로 퍼센티이 값을 저장합니다.
     }
 
+    // TODO runningRecord와 ChallengeWithCondition으로 사용자의 챌린지 성취 기록 반환
     private ChallengeAchievementRecord getChallengeAchievement(
             RunningRecord runningRecord, ChallengeWithCondition challengeDataWithConditions) {
-        boolean allSuccess = true;
-        boolean allHasPercentage = true;
-        PercentageValues percentageValues = null;
+        boolean allSuccess = true; // 전체 조건 성공 시 성공 여부(true)
+        boolean allHasPercentage = true; // 전체 조건에서 하나라도 퍼센테이지가 존재하지 않는 조건이 있으면 false
+        PercentageValues percentageValues = null; // 전체 조건에서 하나라도 퍼센테이지가 존재하지 않는 조건이 있으면 null로
 
+        // 챌린지의 조건에 따라 해당 러닝에서의 챌린지 완수(성취) 기록을 계산합니다.
         for (ChallengeCondition condition : challengeDataWithConditions.challengeConditions()) {
             boolean success = condition.isAchieved(condition.goalType().getActualValue(runningRecord));
 
