da2269d692de0eb9f522c71400128ef9759a4c87
hee9841 <lee98417716@gmail.com>
2024-08-27 16:37:39 +0900

    Rename: findAllChallenges의 인자 값 네이밍을 변경합니다.

--- src/main/java/com/dnd/runus/domain/challenge/ChallengeRepository.java
+++ src/main/java/com/dnd/runus/domain/challenge/ChallengeRepository.java
@@ -3,7 +3,7 @@ package com.dnd.runus.domain.challenge;
 import java.util.List;
 
 public interface ChallengeRepository {
-    List<ChallengeData.Challenge> findAllChallenges(boolean hasPreRecord);
+    List<ChallengeData.Challenge> findAllChallenges(boolean hasYesterdayRecord);
 
     ChallengeData findChallengeWithConditionsByChallengeId(long challengeId);
 }
