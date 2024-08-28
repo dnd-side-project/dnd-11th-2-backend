b0f5e0e69dcbde1454da03437b8109e5e763b98d
Jaewon Lee <jaewonlee.pro@gmail.com>
2024-08-28 11:50:17 +0900

    Refactor: 사용하지 않는 challenge achievement findByRunningId 삭제

--- src/main/java/com/dnd/runus/domain/challenge/achievement/ChallengeAchievementRepository.java
+++ src/main/java/com/dnd/runus/domain/challenge/achievement/ChallengeAchievementRepository.java
@@ -1,10 +1,6 @@
 package com.dnd.runus.domain.challenge.achievement;
 
-import java.util.Optional;
-
 public interface ChallengeAchievementRepository {
 
     ChallengeAchievement save(ChallengeAchievement challengeAchievement);
-
-    Optional<ChallengeAchievement> findByRunningId(Long runningId);
 }
--- src/main/java/com/dnd/runus/infrastructure/persistence/domain/challenge/ChallengeAchievementRepositoryImpl.java
+++ src/main/java/com/dnd/runus/infrastructure/persistence/domain/challenge/ChallengeAchievementRepositoryImpl.java
@@ -2,32 +2,21 @@ package com.dnd.runus.infrastructure.persistence.domain.challenge;
 
 import com.dnd.runus.domain.challenge.achievement.ChallengeAchievement;
 import com.dnd.runus.domain.challenge.achievement.ChallengeAchievementRepository;
-import com.dnd.runus.infrastructure.persistence.jooq.challenge.JooqChallengeRepository;
 import com.dnd.runus.infrastructure.persistence.jpa.challenge.JpaChallengeAchievementRepository;
 import com.dnd.runus.infrastructure.persistence.jpa.challenge.entity.ChallengeAchievementEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class ChallengeAchievementRepositoryImpl implements ChallengeAchievementRepository {
 
     private final JpaChallengeAchievementRepository jpaChallengeAchievementRepository;
-    private final JooqChallengeRepository jooqChallengeRepository;
 
     @Override
     public ChallengeAchievement save(ChallengeAchievement challengeAchievement) {
         return jpaChallengeAchievementRepository
                 .save(ChallengeAchievementEntity.from(challengeAchievement))
                 .toDomain(challengeAchievement.challenge());
-    }
-
-    @Override
-    public Optional<ChallengeAchievement> findByRunningId(Long runningId) {
-        return jpaChallengeAchievementRepository
-                .findByRunningRecordId(runningId)
-                .map(challengeAchievementEntity -> challengeAchievementEntity.toDomain(null));
     }
 }
