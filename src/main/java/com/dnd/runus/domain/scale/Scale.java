0f2e876215c50a7e8655d1223a9142ba6eb27560
hee9841 <lee98417716@gmail.com>
2024-09-03 21:15:26 +0900

    Fix: Scale 도메인 end를 endName으로 변경

--- src/main/java/com/dnd/runus/domain/scale/Scale.java
+++ src/main/java/com/dnd/runus/domain/scale/Scale.java
@@ -1,3 +1,3 @@
 package com.dnd.runus.domain.scale;
 
-public record Scale(long scaleId, String name, int sizeMeter, int index, String startName, String end) {}
+public record Scale(long scaleId, String name, int sizeMeter, int index, String startName, String endName) {}
