f01cc9497d138ab6cf1257c9a492c99bb3cf6ac5
hee9841 <lee98417716@gmail.com>
2024-09-15 19:42:59 +0900

    Feat: 거리, 시간 관련해서 변환하는 값에 관한 constant 클래스 생성

---
+++ src/main/java/com/dnd/runus/global/constant/MetricsConversionFactor.java
@@ -0,0 +1,12 @@
+package com.dnd.runus.global.constant;
+
+public final class MetricsConversionFactor {
+
+    private MetricsConversionFactor() {}
+
+    public static final int SECONDS_PER_HOUR = 3600;
+    public static final int SECONDS_PER_MINUTE = 60;
+    public static final int MINUTE_PER_HOUR = 60;
+
+    public static final double METERS_IN_A_KILOMETER = 1000.0;
+}
