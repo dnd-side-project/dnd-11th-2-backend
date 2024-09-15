87627dd0a87c6b81307bf2e93ff86f9e0b1a199f
이재원 <jaewonlee.pro@gmail.com>
2024-09-15 19:34:30 +0900

    Test: OauthService sign in, up 메서드 테스트 추가 (#197)

--- src/test/java/com/dnd/runus/application/oauth/OauthServiceTest.java
+++ src/test/java/com/dnd/runus/application/oauth/OauthServiceTest.java
@@ -2,6 +2,8 @@ package com.dnd.runus.application.oauth;
 
 import com.dnd.runus.auth.oidc.provider.OidcProvider;
 import com.dnd.runus.auth.oidc.provider.OidcProviderRegistry;
+import com.dnd.runus.auth.token.TokenProviderModule;
+import com.dnd.runus.auth.token.dto.AuthTokenDto;
 import com.dnd.runus.domain.badge.BadgeAchievementRepository;
 import com.dnd.runus.domain.challenge.achievement.ChallengeAchievementPercentageRepository;
 import com.dnd.runus.domain.challenge.achievement.ChallengeAchievementRepository;
@@ -19,8 +21,13 @@ import com.dnd.runus.domain.scale.ScaleAchievementRepository;
 import com.dnd.runus.global.constant.MemberRole;
 import com.dnd.runus.global.constant.RunningEmoji;
 import com.dnd.runus.global.constant.SocialType;
+import com.dnd.runus.global.exception.BusinessException;
 import com.dnd.runus.global.exception.NotFoundException;
+import com.dnd.runus.global.exception.type.ErrorType;
+import com.dnd.runus.presentation.v1.oauth.dto.request.SignInRequest;
+import com.dnd.runus.presentation.v1.oauth.dto.request.SignUpRequest;
 import com.dnd.runus.presentation.v1.oauth.dto.request.WithdrawRequest;
+import com.dnd.runus.presentation.v1.oauth.dto.response.SignResponse;
 import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -39,10 +46,47 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
-import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 class OauthServiceTest {
+    @InjectMocks
+    private OauthService oauthService;
+
+    @Mock
+    private OidcProviderRegistry oidcProviderRegistry;
+
+    @Mock
+    private OidcProvider oidcProvider;
+
+    @Mock
+    private SocialProfileRepository socialProfileRepository;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private TokenProviderModule tokenProviderModule;
+
+    @Mock
+    private MemberLevelRepository memberLevelRepository;
+
+    @Mock
+    private BadgeAchievementRepository badgeAchievementRepository;
+
+    @Mock
+    private RunningRecordRepository runningRecordRepository;
+
+    @Mock
+    private ChallengeAchievementRepository challengeAchievementRepository;
+
+    @Mock
+    private GoalAchievementRepository goalAchievementRepository;
+
+    @Mock
+    private ChallengeAchievementPercentageRepository challengeAchievementPercentageRepository;
+
+    @Mock
+    private ScaleAchievementRepository scaleAchievementRepository;
 
     private Member member;
 
@@ -55,39 +99,123 @@ class OauthServiceTest {
     @ExtendWith(MockitoExtension.class)
     @DisplayName("oauth관련 테스트: 회원가입, 로그인, 탈퇴 관련")
     class OauthTest {
-        @InjectMocks
-        private OauthService oauthService;
-
-        @Mock
-        private OidcProviderRegistry oidcProviderRegistry;
-
-        @Mock
-        private OidcProvider oidcProvider;
-
-        @Mock
-        private MemberRepository memberRepository;
-
-        @Mock
-        private SocialProfileRepository socialProfileRepository;
-
         private SocialType socialType;
         private String idToken;
         private String authorizationCode;
         private String oauthId;
         private String email;
 
+        @Mock
         private Claims claims;
 
         @BeforeEach
         void setUp() {
-
-            claims = mock(Claims.class);
-
             socialType = SocialType.APPLE;
             idToken = "idToken";
             authorizationCode = "authorizationCode";
             oauthId = "oauthId";
             email = "oauthEmail@email.com";
+        }
+
+        @Test
+        @DisplayName("sign-in 시 socialProfile이 있다면 성공")
+        void socialProfile_exist_then_signIn_success() {
+            // given
+            SignInRequest request = new SignInRequest(socialType, idToken);
+            given(oidcProviderRegistry.getOidcProviderBy(socialType)).willReturn(oidcProvider);
+            given(oidcProvider.getClaimsBy(idToken)).willReturn(claims);
+            given(claims.getSubject()).willReturn(oauthId);
+            given(claims.get("email")).willReturn(email);
+            given(socialProfileRepository.findBySocialTypeAndOauthId(socialType, oauthId))
+                    .willReturn(Optional.of(new SocialProfile(1L, member, socialType, oauthId, email)));
+            AuthTokenDto tokenDto = new AuthTokenDto("access-token", "bearer");
+            given(tokenProviderModule.generate(String.valueOf(member.memberId())))
+                    .willReturn(tokenDto);
+
+            // when
+            SignResponse signResponse = oauthService.signIn(request);
+
+            // then
+            assertNotNull(signResponse);
+            assertEquals(member.nickname(), signResponse.nickname());
+            assertEquals(email, signResponse.email());
+            assertEquals(tokenDto.accessToken(), signResponse.accessToken());
+        }
+
+        @Test
+        @DisplayName("sign-in 시 socialProfile이 없다면 에러 타입이 USER_NOT_FOUND인 BusinessException 에러 발생")
+        void socialProfile_not_exist_then_signIn_fail() {
+            // given
+            SignInRequest request = new SignInRequest(socialType, idToken);
+            given(oidcProviderRegistry.getOidcProviderBy(socialType)).willReturn(oidcProvider);
+            given(oidcProvider.getClaimsBy(idToken)).willReturn(claims);
+            given(claims.getSubject()).willReturn(oauthId);
+            given(claims.get("email")).willReturn(email);
+            given(socialProfileRepository.findBySocialTypeAndOauthId(socialType, oauthId))
+                    .willReturn(Optional.empty());
+
+            // when, then
+            BusinessException exception = assertThrows(BusinessException.class, () -> oauthService.signIn(request));
+            assertEquals(ErrorType.USER_NOT_FOUND, exception.getType());
+        }
+
+        @Test
+        @DisplayName("sign-up 시 socialProfile이 있다면 해당 socialProfile을 사용")
+        void socialProfile_not_exist_then_signUp_success() {
+            // given
+            SignUpRequest request = new SignUpRequest(socialType, idToken, "nickname");
+            given(oidcProviderRegistry.getOidcProviderBy(socialType)).willReturn(oidcProvider);
+            given(oidcProvider.getClaimsBy(idToken)).willReturn(claims);
+            given(claims.getSubject()).willReturn(oauthId);
+            given(claims.get("email")).willReturn(email);
+            SocialProfile socialProfile = new SocialProfile(1L, member, socialType, oauthId, email);
+            given(socialProfileRepository.findBySocialTypeAndOauthId(socialType, oauthId))
+                    .willReturn(Optional.of(socialProfile));
+            AuthTokenDto tokenDto = new AuthTokenDto("access-token", "bearer");
+            given(tokenProviderModule.generate(String.valueOf(member.memberId())))
+                    .willReturn(tokenDto);
+
+            // when
+            SignResponse signResponse = oauthService.signUp(request);
+
+            // then
+            assertNotNull(signResponse);
+            assertEquals(member.nickname(), signResponse.nickname());
+            assertEquals(email, signResponse.email());
+            assertEquals(tokenDto.accessToken(), signResponse.accessToken());
+        }
+
+        @Test
+        @DisplayName("sign-up 시 socialProfile이 없다면 socialProfileRepository.save() 호출")
+        void socialProfile_not_exist_then_signUp_save_social_profile() {
+            // given
+            SignUpRequest request = new SignUpRequest(socialType, idToken, "nickname");
+            given(oidcProviderRegistry.getOidcProviderBy(socialType)).willReturn(oidcProvider);
+            given(oidcProvider.getClaimsBy(idToken)).willReturn(claims);
+            given(claims.getSubject()).willReturn(oauthId);
+            given(claims.get("email")).willReturn(email);
+            given(socialProfileRepository.findBySocialTypeAndOauthId(socialType, oauthId))
+                    .willReturn(Optional.empty());
+
+            Member newMember =
+                    new Member(2L, MemberRole.USER, request.nickname(), OffsetDateTime.now(), OffsetDateTime.now());
+            SocialProfile socialProfile = new SocialProfile(1L, newMember, socialType, oauthId, email);
+            given(socialProfileRepository.save(any(SocialProfile.class))).willReturn(socialProfile);
+
+            AuthTokenDto tokenDto = new AuthTokenDto("access-token", "bearer");
+            given(tokenProviderModule.generate(String.valueOf(newMember.memberId())))
+                    .willReturn(tokenDto);
+
+            // when
+            SignResponse signResponse = oauthService.signUp(request);
+
+            // then
+            assertNotNull(signResponse);
+            assertEquals(newMember.nickname(), signResponse.nickname());
+            assertEquals(email, signResponse.email());
+            assertEquals(tokenDto.accessToken(), signResponse.accessToken());
+
+            then(socialProfileRepository).should().save(any(SocialProfile.class));
         }
 
         @DisplayName("소셜 로그인 연동 해제: 성공")
@@ -170,35 +298,6 @@ class OauthServiceTest {
     @ExtendWith(MockitoExtension.class)
     @DisplayName("회원 탈퇴를 위한 멤버 데이터 삭제")
     class DeleteAllMemberTest {
-        @InjectMocks
-        private OauthService oauthService;
-
-        @Mock
-        private MemberRepository memberRepository;
-
-        @Mock
-        private SocialProfileRepository socialProfileRepository;
-
-        @Mock
-        private MemberLevelRepository memberLevelRepository;
-
-        @Mock
-        private BadgeAchievementRepository badgeAchievementRepository;
-
-        @Mock
-        private RunningRecordRepository runningRecordRepository;
-
-        @Mock
-        private ChallengeAchievementRepository challengeAchievementRepository;
-
-        @Mock
-        private GoalAchievementRepository goalAchievementRepository;
-
-        @Mock
-        private ChallengeAchievementPercentageRepository challengeAchievementPercentageRepository;
-
-        @Mock
-        private ScaleAchievementRepository scaleAchievementRepository;
 
         @DisplayName("회원 삭제: 회원이 존재하지 않으면 NotFoundException을 발생하한다.")
         @Test
