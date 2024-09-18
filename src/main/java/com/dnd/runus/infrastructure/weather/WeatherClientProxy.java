01bd67afcd3e2a628c573cc7e78fcba846abb596
Jaewon Lee <jaewonlee.pro@gmail.com>
2024-09-18 22:21:51 +0900

    Feat: WeatherClientProxy 클래스에 CircuitBreaker 추가

---
+++ src/main/java/com/dnd/runus/infrastructure/weather/WeatherClientProxy.java
@@ -0,0 +1,47 @@
+package com.dnd.runus.infrastructure.weather;
+
+import com.dnd.runus.global.constant.WeatherType;
+import com.dnd.runus.global.exception.BusinessException;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Primary
+@Component
+public class WeatherClientProxy implements WeatherClient {
+
+    private final WeatherClient mainWeatherClient;
+    private final WeatherClient fallbackWeatherClient;
+
+    public WeatherClientProxy(
+            @Qualifier("openweathermapWeatherClient") WeatherClient openweathermapWeatherClient,
+            @Qualifier("weatherApiComWeatherClient") WeatherClient weatherApiComWeatherClient) {
+        this.mainWeatherClient = openweathermapWeatherClient;
+        this.fallbackWeatherClient = weatherApiComWeatherClient;
+    }
+
+    @Override
+    @CircuitBreaker(name = "weatherClient", fallbackMethod = "fallback")
+    public WeatherInfo getWeatherInfo(double longitude, double latitude) {
+        return mainWeatherClient.getWeatherInfo(longitude, latitude);
+    }
+
+    public WeatherInfo fallback(double longitude, double latitude, BusinessException e) {
+        log.error("Business exception occurred. type: {}, message: {}", e.getType(), e.getMessage());
+        return fallbackWeatherClient.getWeatherInfo(longitude, latitude);
+    }
+
+    public WeatherInfo fallback(double longitude, double latitude, CallNotPermittedException e) {
+        log.error("Circuit breaker is open. {}", e.getMessage());
+        return fallbackWeatherClient.getWeatherInfo(longitude, latitude);
+    }
+
+    public WeatherInfo fallback(Exception e) {
+        log.error("Fallback occurred. {}", e.getMessage());
+        return new WeatherInfo(WeatherType.CLOUDY, 0, 0, 0, 0, 0);
+    }
+}
