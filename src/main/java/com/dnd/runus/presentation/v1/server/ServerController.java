f166d337dd25d20d70f5181dbab8785a997c98bf
Jaewon Lee <jaewonlee.pro@gmail.com>
2024-09-21 12:46:10 +0900

    Docs: 앱 버전 지원 확인 api 파라미터 예제 추가

--- src/main/java/com/dnd/runus/presentation/v1/server/ServerController.java
+++ src/main/java/com/dnd/runus/presentation/v1/server/ServerController.java
@@ -5,6 +5,7 @@ import com.dnd.runus.application.server.ServerVersionService;
 import com.dnd.runus.application.server.Version;
 import com.dnd.runus.presentation.v1.server.dto.response.VersionStatusResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +23,7 @@ public class ServerController {
 
     @GetMapping("versions")
     @Operation(summary = "앱 버전 지원 여부 확인", description = "기기의 앱 버전을 확인하고 앱 업데이트가 필요한지 여부를 반환합니다.")
+    @Parameter(name = "version", description = "앱 버전", required = true, example = "1.0.0")
     public VersionStatusResponse checkVersion(@RequestParam String version) {
         boolean updateRequired = serverVersionService.isUpdateRequired(Version.parse(version), DeviceType.IOS);
         return new VersionStatusResponse(updateRequired);
