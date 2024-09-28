9f0908e7d833d733c78b541654a8b3aafc08132f
Jaewon Lee <jaewonlee.pro@gmail.com>
2024-09-29 00:23:45 +0900

    Fix: running record 조회 시 challenge나 goal 성취도 같이 조회하도록 수정

--- src/main/java/com/dnd/runus/domain/challenge/achievement/ChallengeAchievementRepository.java
+++ src/main/java/com/dnd/runus/domain/challenge/achievement/ChallengeAchievementRepository.java
@@ -3,12 +3,15 @@ package com.dnd.runus.domain.challenge.achievement;
 import com.dnd.runus.domain.running.RunningRecord;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChallengeAchievementRepository {
 
     ChallengeAchievement save(ChallengeAchievement challengeAchievement);
 
     List<Long> findIdsByRunningRecords(List<RunningRecord> runningRecords);
+
+    Optional<ChallengeAchievement.Status> findByRunningRecordId(long runningRecordId);
 
     void deleteByIds(List<Long> ids);
 }
