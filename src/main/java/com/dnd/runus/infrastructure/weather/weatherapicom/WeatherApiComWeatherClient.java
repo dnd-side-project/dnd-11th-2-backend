67c861a52bdc68d4c0c0babd9e98f870ee2414a4
이재원 <jaewonlee.pro@gmail.com>
2024-09-28 17:32:54 +0900

    Refactor: WeatherApiComWeatherClient.getWeatherInfo 메서드 내 API 2개 동기 호출을 비동기로 전환 (#240)

--- src/main/java/com/dnd/runus/infrastructure/weather/weatherapicom/WeatherApiComWeatherClient.java
+++ src/main/java/com/dnd/runus/infrastructure/weather/weatherapicom/WeatherApiComWeatherClient.java
@@ -6,50 +6,68 @@ import com.dnd.runus.infrastructure.weather.WeatherClient;
 import com.dnd.runus.infrastructure.weather.WeatherInfo;
 import com.dnd.runus.infrastructure.weather.weatherapicom.dto.WeatherapicomCurrent;
 import com.dnd.runus.infrastructure.weather.weatherapicom.dto.WeatherapicomHistory;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 import static com.dnd.runus.global.exception.type.ErrorType.WEATHER_API_ERROR;
-import static org.apache.commons.lang3.ObjectUtils.isEmpty;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
 
+@Slf4j
 @Component
-@RequiredArgsConstructor
 public class WeatherApiComWeatherClient implements WeatherClient {
     private final WeatherapicomWeatherHttpClient weatherapicomWeatherHttpClient;
+    private final String apiKey;
+    private final ExecutorService executorService;
 
-    @Value("${weather.weatherapicom.api-key}")
-    private String apiKey;
+    public WeatherApiComWeatherClient(
+            WeatherapicomWeatherHttpClient weatherapicomWeatherHttpClient,
+            @Value("${weather.weatherapicom.api-key}") String apiKey,
+            @Qualifier("virtualExecutorService") ExecutorService executorService) {
+        this.weatherapicomWeatherHttpClient = weatherapicomWeatherHttpClient;
+        this.apiKey = apiKey;
+        this.executorService = executorService;
+    }
 
     @Override
     public WeatherInfo getWeatherInfo(double longitude, double latitude) {
         String query = String.format("%f,%f", latitude, longitude);
 
-        WeatherapicomCurrent currentInfo = weatherapicomWeatherHttpClient.getWeatherInfo(query, apiKey);
-        if (currentInfo == null || currentInfo.current() == null) {
-            throw new BusinessException(WEATHER_API_ERROR, "[weatherapicom] 현재 날씨 정보를 가져올 수 없습니다.");
+        Future<WeatherapicomCurrent> currentFuture =
+                executorService.submit(() -> weatherapicomWeatherHttpClient.getWeatherInfo(query, apiKey));
+
+        Future<WeatherapicomHistory> historyFuture = executorService.submit(() -> {
+            int hour = 0;
+            String datetime = LocalDate.now().format(ISO_LOCAL_DATE);
+            return weatherapicomWeatherHttpClient.getWeatherHistory(query, datetime, hour, apiKey);
+        });
+
+        try {
+            WeatherapicomCurrent current = currentFuture.get();
+            WeatherapicomHistory history = historyFuture.get();
+
+            double mphToMs = 0.44704;
+
+            WeatherapicomHistory.Forecast.ForecaseDay.DayInfo today =
+                    history.forecast().forecastday().getFirst().day();
+
+            return new WeatherInfo(
+                    mapWeatherType(current.current().condition().code()),
+                    current.current().feelslikeC(),
+                    today.mintempC(),
+                    today.maxtempC(),
+                    current.current().humidity(),
+                    current.current().windMph() * mphToMs);
+        } catch (Exception e) {
+            throw new BusinessException(
+                    WEATHER_API_ERROR,
+                    String.format("[weatherapicom] lon: %f, lat: %f, %s", longitude, latitude, e.getMessage()));
         }
-
-        String datetime = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
-        WeatherapicomHistory historyInfo = weatherapicomWeatherHttpClient.getWeatherHistory(query, datetime, 0, apiKey);
-
-        if (historyInfo == null
-                || historyInfo.forecast() == null
-                || isEmpty(historyInfo.forecast().forecastday())) {
-            throw new BusinessException(WEATHER_API_ERROR, "[weatherapicom] 과거 날씨 정보를 가져올 수 없습니다.");
-        }
-
-        double mphToMs = 0.44704;
-        return new WeatherInfo(
-                mapWeatherType(currentInfo.current().condition().code()),
-                currentInfo.current().feelslikeC(),
-                historyInfo.forecast().forecastday().get(0).day().mintempC(),
-                historyInfo.forecast().forecastday().get(0).day().maxtempC(),
-                currentInfo.current().humidity(),
-                currentInfo.current().windMph() * mphToMs);
     }
 
     private WeatherType mapWeatherType(int weatherCode) {
