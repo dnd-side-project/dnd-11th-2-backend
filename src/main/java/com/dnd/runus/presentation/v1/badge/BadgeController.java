52148bfc659d3beecb0029583d83d7b980fbd9d5
hee9841 <lee98417716@gmail.com>
2024-10-01 21:08:49 +0900

    Feat: 나의 뱃지 목록 조회 컨트롤러 계층 구현

--- src/main/java/com/dnd/runus/presentation/v1/badge/BadgeController.java
+++ src/main/java/com/dnd/runus/presentation/v1/badge/BadgeController.java
@@ -3,6 +3,7 @@ package com.dnd.runus.presentation.v1.badge;
 import com.dnd.runus.application.badge.BadgeService;
 import com.dnd.runus.presentation.annotation.MemberId;
 import com.dnd.runus.presentation.v1.badge.dto.response.AchievedBadgesResponse;
+import com.dnd.runus.presentation.v1.badge.dto.response.AllBadgesListResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,5 +20,19 @@ public class BadgeController {
     @Operation(summary = "자신이 획득한 뱃지 전체 조회", description = "자신이 획득한 뱃지를 전체 조회합니다.")
     public AchievedBadgesResponse getMyBadges(@MemberId long memberId) {
         return badgeService.getAchievedBadges(memberId);
+    }
+
+    @GetMapping("/my-list")
+    @Operation(
+            summary = "나의 뱃지 조회 목록 조회",
+            description =
+                    """
+        서비스의 전체 뱃지를 조회 합니다.<br>
+        자신이 획득한 뱃지는 isAchieved가 true 표시됩니다.<br>
+        최근 일주일에 사용자가 획득한 뱃지는 recencyBadges 리스트에 리턴됩니다.<br>
+        뱃지의 카테고리별 리스트로 뱃지가 리턴되빈다.<br>
+        """)
+    public AllBadgesListResponse getAllBadgesList(@MemberId long memberId) {
+        return badgeService.getListOfAllBadges(memberId);
     }
 }
