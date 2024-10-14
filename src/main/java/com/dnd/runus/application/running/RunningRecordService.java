9300d519c5cf9f81a4eb105dd91c3a2ca1b2a042
hee9841 <lee98417716@gmail.com>
2024-10-14 16:45:29 +0900

    Feat: 러닝 저장 시 평균페이스 계산해서 정장하는 것으로 수정
    - 평균페이스 계산 함수 추가
    - 저장 시 평균페이스 계산 함수 호출 후 평균페이스 계산해서 저장

--- src/main/java/com/dnd/runus/application/running/RunningRecordService.java
+++ src/main/java/com/dnd/runus/application/running/RunningRecordService.java
@@ -9,6 +9,7 @@ import com.dnd.runus.domain.challenge.achievement.ChallengeAchievementPercentage
 import com.dnd.runus.domain.challenge.achievement.ChallengeAchievementRecord;
 import com.dnd.runus.domain.challenge.achievement.ChallengeAchievementRepository;
 import com.dnd.runus.domain.common.Coordinate;
+import com.dnd.runus.domain.common.Pace;
 import com.dnd.runus.domain.goalAchievement.GoalAchievement;
 import com.dnd.runus.domain.goalAchievement.GoalAchievementRepository;
 import com.dnd.runus.domain.level.Level;
@@ -37,6 +38,7 @@ import java.util.List;
 
 import static com.dnd.runus.global.constant.MetricsConversionFactor.METERS_IN_A_KILOMETER;
 import static com.dnd.runus.global.constant.MetricsConversionFactor.SECONDS_PER_HOUR;
+import static com.dnd.runus.global.constant.MetricsConversionFactor.SECONDS_PER_MINUTE;
 import static com.dnd.runus.global.constant.TimeConstant.SERVER_TIMEZONE;
 
 @Service
@@ -179,7 +181,9 @@ public class RunningRecordService {
                 .distanceMeter(request.runningData().distanceMeter())
                 .duration(request.runningData().runningTime())
                 .calorie(request.runningData().calorie())
-                .averagePace(request.runningData().averagePace())
+                .averagePace(calAvgPace(
+                        request.runningData().distanceMeter(),
+                        request.runningData().runningTime()))
                 .route(route)
                 .build());
 
@@ -279,5 +283,14 @@ public class RunningRecordService {
 
         GoalAchievement goalAchievement = new GoalAchievement(runningRecord, goalMetricType, goalValue, isAchieved);
         return goalAchievementRepository.save(goalAchievement);
+    }
+
+    private Pace calAvgPace(int distanceMeter, Duration runningTime) {
+        double paceOfSecondPerMeter = (double) runningTime.toSeconds() / distanceMeter;
+        double paceOfMinutePerKilometer = (paceOfSecondPerMeter * METERS_IN_A_KILOMETER) / SECONDS_PER_MINUTE;
+        int minute = (int) Math.floor(paceOfMinutePerKilometer);
+        int second = (int) Math.floor((paceOfMinutePerKilometer - minute) * SECONDS_PER_MINUTE);
+
+        return new Pace(minute, second);
     }
 }
