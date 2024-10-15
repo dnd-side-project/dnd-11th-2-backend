06bc12bc85411c02b61ba90f8c5edc7f9103b72c
Jaewon Lee <jaewonlee.pro@gmail.com>
2024-10-15 10:53:21 +0900

    Fix: JpaRunningRecordRepository.findByMemberIdAndStartAtBetween 메서드에서 member 테이블을 fetch join하도록 수정

--- src/main/java/com/dnd/runus/infrastructure/persistence/jpa/running/JpaRunningRecordRepository.java
+++ src/main/java/com/dnd/runus/infrastructure/persistence/jpa/running/JpaRunningRecordRepository.java
@@ -1,6 +1,7 @@
 package com.dnd.runus.infrastructure.persistence.jpa.running;
 
 import com.dnd.runus.infrastructure.persistence.jpa.running.entity.RunningRecordEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.OffsetDateTime;
@@ -10,10 +11,12 @@ public interface JpaRunningRecordRepository extends JpaRepository<RunningRecordE
 
     void deleteByMemberId(long memberId);
 
+    @EntityGraph(attributePaths = {"member"})
     List<RunningRecordEntity> findByMemberIdAndStartAtBetween(
             long memberId, OffsetDateTime startTime, OffsetDateTime endTime);
 
     boolean existsByMemberIdAndStartAtBetween(long memberId, OffsetDateTime startTime, OffsetDateTime endTime);
 
+    @EntityGraph(attributePaths = {"member"})
     List<RunningRecordEntity> findByMemberId(long memberId);
 }
