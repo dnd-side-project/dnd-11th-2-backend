9b5a534769f9c5a197766fabb65f8065172325c4
hee9841 <lee98417716@gmail.com>
2024-10-24 20:14:55 +0900

    Fix: 뱃지 성취 날짜 응답값에 추가
    - BadgeWithAchievedStatus에 LocalDateTime achievedAt 값 추가
    - BadgeWithAchieveStatusAndAchievedAt의 achievedAt 타입 OffsetDateTime에서 LocalDateTime으로 변경
    - BadgeWithAchieveStatusAndAchievedAt의 achievedAt 타입변경으로 서비스 단 isRecent함수 criterionDate 인자타입 LocalDateTime으로 변경

--- src/main/java/com/dnd/runus/domain/badge/BadgeWithAchieveStatusAndAchievedAt.java
+++ src/main/java/com/dnd/runus/domain/badge/BadgeWithAchieveStatusAndAchievedAt.java
@@ -2,10 +2,11 @@ package com.dnd.runus.domain.badge;
 
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
-public record BadgeWithAchieveStatusAndAchievedAt(@NotNull Badge badge, boolean isAchieved, OffsetDateTime achievedAt) {
+public record BadgeWithAchieveStatusAndAchievedAt(@NotNull Badge badge, boolean isAchieved, LocalDateTime achievedAt) {
     public BadgeWithAchieveStatusAndAchievedAt(Badge badge, OffsetDateTime achievedAt) {
-        this(badge, achievedAt != null, achievedAt);
+        this(badge, achievedAt != null, achievedAt != null ? achievedAt.toLocalDateTime() : null);
     }
 }
