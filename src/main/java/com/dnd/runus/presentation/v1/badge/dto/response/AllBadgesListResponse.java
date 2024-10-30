e6c71ae0316144237e7a4e279520288b62e3d4e1
hee9841 <lee98417716@gmail.com>
2024-10-30 23:02:49 +0900

    Docs: 나의 뱃지 목록 조회 API 문서 수정

--- src/main/java/com/dnd/runus/presentation/v1/badge/dto/response/AllBadgesListResponse.java
+++ src/main/java/com/dnd/runus/presentation/v1/badge/dto/response/AllBadgesListResponse.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 public record AllBadgesListResponse(
+    @Schema(description = "최신 뱃지 목록(일주일 기준), 없으면 빈리스트 리턴")
     List<AchievedBadge> recencyBadges,
     List<BadgesWithType> badgesList
 ) {
