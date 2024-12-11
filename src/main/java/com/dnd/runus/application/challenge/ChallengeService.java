e8a53ac7e156f299ba29eabe2e19e5b5e4ff73ea
hee9841 <lee98417716@gmail.com>
2024-12-11 17:56:39 +0900

    Fix: 어제 기록관련 if 처리 수정, 챌린지 목표값 추가 관련해서 for문에서 스트림으로 변경

--- src/main/java/com/dnd/runus/application/challenge/ChallengeService.java
+++ src/main/java/com/dnd/runus/application/challenge/ChallengeService.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 import static com.dnd.runus.global.constant.TimeConstant.SERVER_TIMEZONE_ID;
 
@@ -37,31 +36,25 @@ public class ChallengeService {
         List<ChallengeWithCondition> allChallengesWithConditions =
                 new ArrayList<>(challengeRepository.findAllActiveChallengesWithConditions());
 
-        // 어제 기록이 없으면
+        // 어제 기록이 없으면 어제 기록과 관련된 챌린지 삭제
         if (runningRecords.isEmpty()) {
-            allChallengesWithConditions = allChallengesWithConditions.stream()
+            allChallengesWithConditions.removeIf(
+                    challengeWithCondition -> challengeWithCondition.challenge().isDefeatYesterdayChallenge());
+        } else {
+            // 어제의 기록과 관련된 챌린지면, 챌린지 비교할 값(성공 유무를 위한 목표 값) 재등록
+            allChallengesWithConditions.stream()
                     .filter(challengeWithCondition ->
-                            !challengeWithCondition.challenge().isDefeatYesterdayChallenge())
-                    .collect(Collectors.toList());
+                            challengeWithCondition.challenge().isDefeatYesterdayChallenge())
+                    .forEach(challengeWithCondition -> challengeWithCondition
+                            .conditions()
+                            .forEach(condition -> condition.registerComparisonValue(
+                                    condition.goalMetricType().getActualValue(runningRecords.getFirst()))));
         }
 
         // 랜덤으로 2개 리턴
         Random randomWithSeed = new Random(todayMidnight.toEpochSecond());
         Collections.shuffle(allChallengesWithConditions, randomWithSeed);
-        List<ChallengeWithCondition> randomChallengeWithConditions = allChallengesWithConditions.subList(0, 2);
 
-        if (!runningRecords.isEmpty()) {
-            for (ChallengeWithCondition challenge : randomChallengeWithConditions) {
-                if (challenge.challenge().isDefeatYesterdayChallenge()) {
-                    // 어제의 기록과 관련된 챌린지면, 챌린지 비교할 값(성공 유무를 위한 목표 값) 재등록
-                    challenge
-                            .conditions()
-                            .forEach(condition -> condition.registerComparisonValue(
-                                    condition.goalMetricType().getActualValue(runningRecords.getFirst())));
-                }
-            }
-        }
-
-        return randomChallengeWithConditions;
+        return allChallengesWithConditions.subList(0, 2);
     }
 }
